77e2a599e460a8a67105c34b8085eb67d01f2f7a
Emile <emile.peeters@student.howest.be>
2025-12-23 22:34:17 +0100

    Update main.py: optie1 toegevoegd

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
+from database import Database
+
 def show_menu():
     print("\n")
     print("##########################################")
     print("#  1) Overzicht                          #")
     print("#  2) Brommer toevoegen                  #")
     print("#  3) Brommer wijzigen                   #")
-    print("#  3) Brommer wijzigen                   #")
     print("#  4) Brommer verwijderen/verkocht       #")
     print("#  5) Brommer zoeken op merk of model    #")
     print("#  6) Overzicht printen naar een file    #")
     print("#  0) Exit                               #")
-    print("##########################################")
-    
+    print("##########################################")  
 def main():
+    db = Database()
     while True:
         show_menu()
         print("\n")
         choice = input("Keuze: ")
 
         if choice == '1':
-            break
+            print("------------------------------------------")
+            print("OVERZICHT")
+            cursor = db.execute(
+                "SELECT id, merk, model, productiedatum, prijs, vinnummer FROM brommers"
+            )
+            for b in cursor.fetchall():
+                print(f"ID: {b[0]} | Merk: {b[1]} | Model: {b[2]} | "
+                      f"Productiedatum: {b[3]} | Prijs: €{b[4]} | VIN: {b[5]}")
+
+            
         elif choice == '2':   
             break
         elif choice == '3':
